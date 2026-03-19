89fdd9f57c0c5984941586a33ea2bf1a78a481ac
David Hong <dhong47@gatech.edu>
2026-03-19 19:48:45 -0400

    improved the prompt for the llm

--- backend/rag_retrieval.py
+++ backend/rag_retrieval.py
@@ -3,21 +3,40 @@ import os
 from langchain_classic.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 from backend.config import OPENAI_API_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+def build_chain():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-vectorstore = Chroma(persist_directory=os.path.join(BASE_DIR, "db"), embedding_function= embeddings)
+    vectorstore = Chroma(persist_directory=os.path.join(BASE_DIR, "db"), embedding_function= embeddings)
 
-retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
-llm = ChatOpenAI(model="gpt-4o-mini", openai_api_key = OPENAI_API_KEY)
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+    llm = ChatOpenAI(model="gpt-4o-mini", openai_api_key = OPENAI_API_KEY)
 
-qa_chain = RetrievalQA.from_chain_type(llm = llm, retriever = retriever)
+    prompt_template = """
+    You are a professional fitness coach.
 
+    Use the following context to answer the question.
+    If you don't know, give your best helpful fitness advice anyway.
 
-if __name__ == "__main__":
-    result = qa_chain.invoke("What are the primary muscles for bench press?")
-    print(result)
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer:
+    """
+
+    PROMPT = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+
+    qa_chain = RetrievalQA.from_chain_type(llm = llm, retriever = retriever, chain_type_kwargs= {'prompt': PROMPT})
+
+    return qa_chain
--- frontend/app.py
+++ frontend/app.py
@@ -4,30 +4,44 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import streamlit as st
-from backend.rag_retrieval import qa_chain
+from backend.rag_retrieval import build_chain
 
-@st.cache_resource
-def load_chain():
-    from backend.rag_retrieval import qa_chain
-    return qa_chain
+# @st.cache_resource
+# def load_chain():
+#     from backend.rag_retrieval import qa_chain
+#     return qa_chain
 
-qa_chain = load_chain()
+# qa_chain = load_chain()
 
-st.set_page_config(page_title="AI Workout Coach")
+qa_chain = build_chain()
+
+st.set_page_config(page_title="AI Workout Coach", layout="centered")
 
 st.title("💪 AI Workout Coach")
 
-if "history" not in st.session_state:
-    st.session_state.history = []
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-question = st.text_input("Ask your workout coach anything:")
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-if st.button("Ask") and question:
-    response = qa_chain.invoke(question)
+if prompt := st.chat_input("Ask your workout coach anything..."):
+    
+    # Save user message
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Get AI response
+    response = qa_chain.invoke(prompt)
     answer = response["result"]
 
-    st.session_state.history.append((question, answer))
+    # Display AI response
+    with st.chat_message("assistant"):
+        st.markdown(answer)
 
-for q, a in reversed(st.session_state.history):
-    st.markdown(f"**You:** {q}")
-    st.markdown(f"**Coach:** {a}")
+    # Save AI response
+    st.session_state.messages.append({"role": "assistant", "content": answer})
